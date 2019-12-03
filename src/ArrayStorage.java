7031eb28ab40bec66757b0e2e70933416ee9a50e
marinaBk <49858102+marinaBk@users.noreply.github.com>
2019-12-03 15:13:56 +0200

    First task is ready!
    Version 1.0

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,69 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+   private  Resume[] storage = new Resume[10000];
+   private int _size = 0;
 
     void clear() {
+        for (int i = 0; i < _size; i++) {
+                storage[i] = null;
+        }
+        _size=0;
     }
 
     void save(Resume r) {
+        if (r != null) {
+            storage[_size] = new Resume();
+            storage[_size].uuid = r.uuid;
+            _size++;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume emptyR = null;
+
+        if (uuid != null) {
+            for (int i = 0; i < _size; i++) {
+                if (uuid.equals(storage[i].uuid)) {
+                    return storage[i];
+                }
+            }
+            emptyR = new Resume();
+            emptyR.uuid = uuid.concat(" is absent!");
+        }
+
+        return emptyR; //or return null;
     }
 
     void delete(String uuid) {
+   if (uuid !=null)
+        for (int i = 0; i < _size ; i++) {
+            if(uuid.equals(storage[i].uuid)) {
+                for (int j = i; j < _size; j++) {
+                    if (j==_size-1)
+                        storage[j] = null;
+                    else
+                        storage[j] = storage[j+1];
+                }
+                _size--;
+               break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+     Resume[] newArray = new Resume[_size];
+        System.arraycopy(storage, 0, newArray , 0, _size);
+        return newArray;
     }
 
     int size() {
-        return 0;
+        return _size;
     }
 }
